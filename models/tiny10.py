de4becfcbab20ce255604a510ef1b22379e12c86
matszang <reaper_munky@hotmail.com>
2020-11-13 21:26:47 +0100

    Add KNLH-tiny10-arch

---
+++ models/tiny10.py
@@ -0,0 +1,134 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from foundations import hparams
+from lottery.desc import LotteryDesc
+from models import base
+from pruning import sparse_global
+
+
+class Model(base.Model):
+    """Tiny10 used in KNLH"""
+
+    def __init__(self, plan, initializer, outputs=None):
+        super(Model, self).__init__()
+        outputs = outputs or 10
+        self.net = nn.Sequential(
+            #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding
+            nn.Conv2d(3, 16, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Conv2d(16, 16, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+
+            nn.Conv2d(16, 32, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            
+            nn.Conv2d(32, 32, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+
+            nn.Conv2d(32, 64, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+
+            nn.Conv2d(64, 64, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),            
+            nn.Conv2d(64, 64, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+        )
+        
+        # Final fc layer. Size = number of filters in last segment.
+        self.fc = nn.Linear(plan, outputs)
+        self.criterion = nn.CrossEntropyLoss()
+
+        # Initialize.
+        self.apply(initializer)
+
+    def forward(self, x):
+        out = self.net(x)
+        out = F.avg_pool2d(out, out.size()[3])
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+    @property
+    def output_layer_names(self):
+        return ['fc.weight', 'fc.bias']
+
+    @staticmethod
+    def is_valid_model_name(model_name):
+        return (model_name.startswith('tiny10'))
+
+    @staticmethod
+    def get_model_from_name(model_name, initializer,  outputs=10):
+        """The naming scheme for a ResNet is 'cifar_resnet_N[_W]'.
+
+        The ResNet is structured as an initial convolutional layer followed by three "segments"
+        and a linear output layer. Each segment consists of D blocks. Each block is two
+        convolutional layers surrounded by a residual connection. Each layer in the first segment
+        has W filters, each layer in the second segment has 32W filters, and each layer in the
+        third segment has 64W filters.
+
+        The name of a ResNet is 'cifar_resnet_N[_W]', where W is as described above.
+        N is the total number of layers in the network: 2 + 6D.
+        The default value of W is 16 if it isn't provided.
+
+        For example, ResNet-20 has 20 layers. Exclusing the first convolutional layer and the final
+        linear layer, there are 18 convolutional layers in the blocks. That means there are nine
+        blocks, meaning there are three blocks per segment. Hence, D = 3.
+        The name of the network would be 'cifar_resnet_20' or 'cifar_resnet_20_16'.
+        """
+
+        if not Model.is_valid_model_name(model_name):
+            raise ValueError('Invalid model name: {}'.format(model_name))
+
+        plan = 64
+
+        return Model(plan, initializer, outputs)
+
+    @property
+    def loss_criterion(self):
+        return self.criterion
+
+    @staticmethod
+    def default_hparams():
+        model_hparams = hparams.ModelHparams(
+            model_name='tiny10',
+            model_init='kaiming_normal',
+            batchnorm_init='uniform',
+        )
+
+        dataset_hparams = hparams.DatasetHparams(
+            dataset_name='cifar10',
+            batch_size=128,
+        )
+
+        training_hparams = hparams.TrainingHparams(
+            optimizer_name='sgd',
+            momentum=0.9,
+            milestone_steps='80ep,120ep',
+            lr=0.1,
+            gamma=0.1,
+            weight_decay=1e-4,
+            training_steps='160ep',
+        )
+
+        pruning_hparams = sparse_global.PruningHparams(
+            pruning_strategy='sparse_global',
+            pruning_fraction=0.2
+        )
+
+        return LotteryDesc(model_hparams, dataset_hparams, training_hparams, pruning_hparams)
