e6957d846b2cc485caa0ab14e1fb29ae7ce63f6e
matszang <reaper_munky@hotmail.com>
2020-11-16 19:38:40 +0100

    Refactor distances + cka

---
+++ compute_cka.py
@@ -0,0 +1,39 @@
+import glob
+import sys
+import ntpath
+import collections
+import re
+import numpy as np
+import os
+
+experiment_id = sys.argv[1]
+distances_dir = '../storage/distances/experiment_{}/'.format(experiment_id)
+with open(distances_dir + 'experiment_{}.txt'.format(experiment_id)) as f:
+  #saved number of layers as first line...
+  n_layers = int(f.readline())
+print(n_layers + " layers.")
+
+def cka(centered_gram_x, centered_gram_y):
+  scaled_hsic = centered_gram_x.ravel().dot(centered_gram_y.ravel())
+
+  normalization_x = np.linalg.norm(centered_gram_x)
+  normalization_y = np.linalg.norm(centered_gram_y)
+  return scaled_hsic / (normalization_x * normalization_y)
+
+#Compute CKA.
+cka_matrix = np.zeros((n_layers, n_layers))
+for layer_0 in range(n_layers):
+    distances_0 = np.load(distances_dir + 'distances_0_layer_{}.npy'.format(layer_0))
+    for layer_1 in range(layer_0, n_layers):
+        print("Loading data for layer {}...".format(layer_1))
+        distances_1 = np.load(distances_dir + 'distances_1_layer_{}.npy'.format(layer_1))
+
+        print("Computing CKA of layers {} vs {}".format(layer_0, layer_1))
+        cka_matrix[layer_0, layer_1] = cka(distances_0, distances_1)
+
+        print('CKA on layers {}, {} = {}'.format(layer_0, layer_1, cka_matrix[layer_0, layer_1]))
+print(cka_matrix)
+np.save(distances_dir + 'cka_matrix.npy', cka_matrix)
+
+with open(distances_dir + 'experiment_{}.txt'.format(experiment_id), "a") as exp_placeholder:
+    exp_placeholder.write("\n" + "CKA computed.")
--- compute_distances.py
+++ compute_distances.py
@@ -19,14 +19,6 @@ def distances(x):
     return center_gram(D)
   
 
-def cka(centered_gram_x, centered_gram_y):
-  scaled_hsic = centered_gram_x.ravel().dot(centered_gram_y.ravel())
-
-  normalization_x = np.linalg.norm(centered_gram_x)
-  normalization_y = np.linalg.norm(centered_gram_y)
-  return scaled_hsic / (normalization_x * normalization_y)
-
-
 #Center symmetric matrix.
 def center_gram(gram):
   if not np.allclose(gram, gram.T):
@@ -61,17 +53,17 @@ def load_paths(basedir1, basedir2, level):
 #Parameters: Number of layers, level of pruning.
 level = 0
 paths = load_paths(sys.argv[1], sys.argv[2], level = level)
-n_layers = 1#max(paths[0].keys())
+n_layers = 2#max(paths[0].keys())
 
 
 #SETUP environment - directory, experiment ids.
 distances_basedir = '../storage/distances/'
 assert os.path.isdir(distances_basedir), '{} is not a valid base directory'.format(distances_basedir)
 #Create experiment directory.
-i = 0
-while os.path.isdir(distances_basedir + "experiment_{}/".format(i)):
-    i += 1
-distances_dir = distances_basedir + "experiment_{}/".format(i)
+exp_id = 0
+while os.path.isdir(distances_basedir + "experiment_{}/".format(exp_id)):
+    exp_id += 1
+distances_dir = distances_basedir + "experiment_{}/".format(exp_id)
 try:
     os.mkdir(distances_dir)
 except OSError:
@@ -79,34 +71,21 @@ except OSError:
 else:
     print("Created {}".format(distances_dir))
 #Save IDs.
-with open(distances_dir + 'experiment_{}.txt'.format(i), "w+") as experiment_id:
-    experiment_id.write("Computing on first {} layers, for experiments \n".format(n_layers))
-    experiment_id.write(sys.argv[1] + "\n")
-    experiment_id.write(sys.argv[2])
+with open(distances_dir + 'experiment_{}.txt'.format(exp_id), "w+") as exp_placeholder:
+    experiment_info = '{}\n'.format(n_layers)+\
+                      "Computing distances on first {} layers, on level {}, for experiments: \n".format(n_layers, level)+\
+                      sys.argv[1] + '\n' + \
+                      sys.argv[2] + '\n'
+    exp_placeholder.write(experiment_info)
 
 
 #Compute Centered distances.
 for layer in range(n_layers):
     for i in range(2):
         layer_activations = np.load(paths[i][layer])
-        print("Computing distances of dataset {}, layer {}...".format(i, layer))
+        print("Computing distances of dataset {}, layer {} of shape {}...".format(i, layer, layer_activations.shape))
         activation_distances = distances(layer_activations)
         np.save(distances_dir + 'distances_{}_layer_{}.npy'.format(i, layer), activation_distances)
 
-#Compute CKA.
-cka_matrix = np.zeros((n_layers, n_layers))
-for layer_0 in range(n_layers):
-    distances_0 = np.load(distances_dir + 'distances_0_layer_{}.npy'.format(layer_0))
-    for layer_1 in range(layer_0, n_layers):
-        print("Loading data for layer {}...".format(layer_1))
-        distances_1 = np.load(distances_dir + 'distances_1_layer_{}.npy'.format(layer_1))
-
-        print("Computing CKA of layers {} vs {}".format(layer_0, layer_1))
-        cka_matrix[layer_0, layer_1] = cka(distances_0, distances_1)
-
-        print('CKA on layers {}, {} = {}'.format(layer_0, layer_1, cka_matrix[layer_0, layer_1]))
-print(cka_matrix)
-np.save(distances_dir + 'cka_matrix.npy', cka_matrix)
-
-with open(distances_dir + 'experiment_{}.txt'.format(i), "w+") as experiment_id:
-    experiment_id.write("Experiment finished.")
+with open(distances_dir + 'experiment_{}.txt'.format(exp_id), "a") as exp_placeholder:
+    exp_placeholder.write("Distances computed.")
