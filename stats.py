1c80888ab10a3bf764e7e0e15e85965c6e731ecb
matszang <reaper_munky@hotmail.com>
2020-11-16 15:39:05 +0100

    Add cka

--- stats.py
+++ stats.py
@@ -4,68 +4,109 @@ import ntpath
 import collections
 import re
 import numpy as np
+import os
 
 
 np.set_printoptions(threshold=sys.maxsize)
 
-basedir = sys.argv[1]
-
-
-outs = collections.defaultdict(lambda: collections.defaultdict(str))
-files = glob.glob(basedir + "/**/lottery_branch_save_activations*/*.act.npy")
-for filepath in files:
-    key = ntpath.basename(filepath)
-    res = re.match(r'.*level_(?P<level>[0-9]+).*', filepath)
-    level = int(res.group('level'))
-    outs[key][level] = filepath
-
-def dist(a, b):
-    h = np.sum(np.abs(a-b))
-    return h
-
-def dist2(a, b):
-    h = np.sum(np.abs(a*b - a))
-    return h
-
-
-
-
-for name, out_seq in outs.items():
-    s_a = []
+# Compute euclidean distances between N examples.
+# Input: N x ..., Output: N x N.
+def distances(x):
+    x_vector = np.reshape(x, (x.shape[0],-1))
     
-    totals = []
-    changed = []
+    D = x_vector.dot(x_vector.T)
+    print("Centering...")
+    return center_gram(D)
+  
 
-    for i in range(20):
-        fi = out_seq[i]
-        fi_data = np.load(fi)
-        perc = np.percentile(fi_data, 75)
-        perc = 0.01
-        print("perc: " ,perc)
+def cka(centered_gram_x, centered_gram_y):
+  scaled_hsic = centered_gram_x.ravel().dot(centered_gram_y.ravel())
 
-        #print(np.histogram(fi_data, bins=20))
-        fi_data[fi_data<=perc] = 0.0
-        fi_data[fi_data>perc] = 1.0
-        print("count nonzero", np.count_nonzero(fi_data))
-        s_a.append(fi_data)
+  normalization_x = np.linalg.norm(centered_gram_x)
+  normalization_y = np.linalg.norm(centered_gram_y)
+  return scaled_hsic / (normalization_x * normalization_y)
 
-        """
-        fi_mask = fi.replace('/model.', '/').replace('.act.npy', '.weight.mask.npy')
-        weight_mask = np.load(fi_mask).transpose()
-        act_mask = np.sum(weight_mask, axis=0)
-        #print(act_mask)
-        print("am",np.count_nonzero(act_mask == 0))
-        """
 
-        #s = np.array(s_a)
-        #for threshold in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    if True:
-        st = np.copy(s_a)
-        res = np.zeros((20, 20))
-        for i in range(20):
-            for o in range(20):
-                #print("cnt i,o", np.count_nonzero(st[i]), np.count_nonzero(st[o]))
-                res[i,o] = dist(st[i], st[o])
+#Center symmetric matrix.
+def center_gram(gram):
+  if not np.allclose(gram, gram.T):
+    raise ValueError('Input must be a symmetric matrix.')
+  gram = gram.copy()
 
-        print(name)
-        print(res[19])
+  means = np.mean(gram, 0, dtype=np.float64)
+  means -= np.mean(means) / 2
+  gram -= means[:, None]
+  gram -= means[None, :]
+
+  return gram
+
+#Load paths in a 2 element list of dictionaries.
+#Each dictionary maps i to the path of activations of the i'th layer (of level=level).
+def load_paths(basedir1, basedir2, level):
+    basedirs = [basedir1, basedir2]
+    layer_to_path = []
+    for i in range(2):
+        layer_to_path.append(collections.defaultdict(lambda: collections.defaultdict(str)))
+        files = glob.glob(basedirs[i] + "/**/lottery_branch_save_activations*/*.act.npy")
+        for filepath in files:
+            res = re.match(r'.*level_(?P<level>[0-9]+).*net.(?P<net>[0-9]+).*', filepath)
+            if res and int(res.group('level')) == level:
+                layer = int(res.group('net'))
+                layer_to_path[i][layer] = filepath
+    return layer_to_path
+
+
+#MAIN
+
+#Parameters: Number of layers, level of pruning.
+level = 0
+paths = load_paths(sys.argv[1], sys.argv[2], level = level)
+n_layers = 1#max(paths[0].keys())
+
+
+#SETUP environment - directory, experiment ids.
+distances_basedir = '../storage/distances/'
+assert os.path.isdir(distances_basedir), '{} is not a valid base directory'.format(distances_basedir)
+#Create experiment directory.
+i = 0
+while os.path.isdir(distances_basedir + "experiment_{}/".format(i)):
+    i += 1
+distances_dir = distances_basedir + "experiment_{}/".format(i)
+try:
+    os.mkdir(distances_dir)
+except OSError:
+    print("Failed to create {}.".format(distances_dir))
+else:
+    print("Created {}".format(distances_dir))
+#Save IDs.
+with open(distances_dir + 'experiment_{}.txt'.format(i), "w+") as experiment_id:
+    experiment_id.write("Computing on first {} layers, for experiments \n".format(n_layers))
+    experiment_id.write(sys.argv[1] + "\n")
+    experiment_id.write(sys.argv[2])
+
+
+#Compute Centered distances.
+for layer in range(n_layers):
+    for i in range(2):
+        layer_activations = np.load(paths[i][layer])
+        print("Computing distances of dataset {}, layer {}...".format(i, layer))
+        activation_distances = distances(layer_activations)
+        np.save(distances_dir + 'distances_{}_layer_{}.npy'.format(i, layer), activation_distances)
+
+#Compute CKA.
+cka_matrix = np.zeros((n_layers, n_layers))
+for layer_0 in range(n_layers):
+    distances_0 = np.load(distances_dir + 'distances_0_layer_{}.npy'.format(layer_0))
+    for layer_1 in range(layer_0, n_layers):
+        print("Loading data for layer {}...".format(layer_1))
+        distances_1 = np.load(distances_dir + 'distances_1_layer_{}.npy'.format(layer_1))
+
+        print("Computing CKA of layers {} vs {}".format(layer_0, layer_1))
+        cka_matrix[layer_0, layer_1] = cka(distances_0, distances_1)
+
+        print('CKA on layers {}, {} = {}'.format(layer_0, layer_1, cka_matrix[layer_0, layer_1]))
+print(cka_matrix)
+np.save(distances_dir + 'cka_matrix.npy', cka_matrix)
+
+with open(distances_dir + 'experiment_{}.txt'.format(i), "w+") as experiment_id:
+    experiment_id.write("Experiment finished.")
